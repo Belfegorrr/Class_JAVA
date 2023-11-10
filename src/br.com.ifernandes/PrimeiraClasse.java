e7b873a1a9b59ac2bf60abef4c675d4544e53a9a
Belfegorrr <pelichek.iago@gmail.com>
2023-11-10 13:45:01 -0300

    Primeira Class Java

---
+++ src/br.com.ifernandes/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package br.com.ifernandes;
+
+/**
+ * @author iago.fernandes
+ */
+public class PrimeiraClasse {
+    public static void main(String args[]) {
+        System.out.print("Hello World!!");
+    }
+}
